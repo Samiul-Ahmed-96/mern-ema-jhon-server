fab88da406700b286c25b3a0b03757d8c6f94968
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-25 21:41:59 +0600

    Initial setup

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iy3km.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected')
+        const database = client.db('online_shop');
+        const productCollection = database.collection('products');
+        
+
+       
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Ema jon server is running');
+});
+
+app.listen(port, () => {
+    console.log('Server running at port', port);
+})
